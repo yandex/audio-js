d8baaa8d0dc4772149f4eace5fc9b702b16cae3f
Mikhail Silaev <gheljenor@yandex-team.ru>
2015-09-21 14:58:56 +0300

    Подключение библиотеки с помощью modules

--- src/modules.js
+++ src/modules.js
@@ -1,11 +1,15 @@
 var Modules = require('ym');
 var YandexAudio = require("./index.js");
 
+if (!window.ya) {
+    window.ya = {};
+}
+
 var modules;
-if (window.modules) {
-    modules = window.modules;
+if (window.ya.modules) {
+    modules = window.ya.modules;
 } else {
-    modules = window.modules = Modules.create();
+    modules = window.ya.modules = Modules.create();
 }
 
 modules.define('YandexAudio', function(provide) {
